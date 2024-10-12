40d99810db4873dfbd31cceb0aeec8687c3dcd68
Remzi85 <ramzila@code.edu.az>
2024-10-12 12:33:32 +0400

    added task1

---
+++ homework_task1/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace homework_task1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 100; i++) {
+                if (i%3==0 && i%5==0)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+    }
+}
